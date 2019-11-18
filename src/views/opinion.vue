64dd78068ea446fb8f4bbf5a9af9bbfe712509f4
javyin <343713@qq.com>
2019-11-18 20:46:16 +0800

    意见反馈完成

--- src/views/opinion.vue
+++ src/views/opinion.vue
@@ -15,7 +15,7 @@
         <span>用户昵称 :</span>
         <input type="text" placeholder="微信昵称" v-model="value1">
       </div>
-      <div>查询结果</div>
+      <div @click="search">查询结果</div>
     </div>
     <div class="content">
             <!-- 表格 -->
@@ -37,7 +37,7 @@
         <el-table-column prop="name" label="操作" width="139" align=center>
            <template slot-scope="scope">
             <el-button
-              @click.native.prevent="deleteRow(scope.$index, tableData4)"
+              @click="handleBtn(scope.row)"
               type="text"
               size="small"
             >
@@ -58,6 +58,17 @@
           :total="total - 0">
     </el-pagination>
   </div>
+    </div>
+    <div class="mask" v-show="isShow">
+        <div class="popup">
+            <h1>意见建议</h1>
+            <span>
+                {{message}}
+            </span>
+            <div>
+                <div @click="cancel">关闭</div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -130,7 +141,8 @@ export default {
       total: "",
       pagesize: "",
       pagenum:1,
-      page: ""
+      page: "",
+      isShow: false
     }
   },
   methods: {
@@ -138,14 +150,41 @@ export default {
     handleSizeChange(val) {
       // console.log(val);
       this.pagesize = val;
-      this.getAllList();
+      this.search();
     },
     //切换当前页码时触发
     handleCurrentChange(val) {
       // console.log(val);
       this.pagenum = val;
-      this.getAllList()
+      this.search()
     },
+    search() {
+        this.$axios({
+            url: '/api/admin/feedback',
+            method: 'get',
+            params: {
+                page: this.pagenum,
+                page_rows: this.pagesize,
+                nickname: this.value1,
+                phone: this.value
+            }
+        }).then(res=>{
+            console.log(res);
+            this.tableData = res.data.data.list
+        })
+    },
+    handleBtn(data) {
+        console.log(data);
+        this.isShow = true;
+        this.message = data.body;
+    },
+    cancel() {
+        this.isShow = false;
+        this.message = '';
+    }
+  },
+  mounted() {
+      this.search()
   }
 }
 </script>
@@ -153,6 +192,48 @@ export default {
 <style lang="less" scoped>
 .opinion{
   padding: 20px;
+  .mask {
+      position: fixed;
+      width: 100%;
+      top:0;
+      bottom: 0;
+      z-index: 100;
+      .popup{
+    width: 300px;
+    height:300px;
+    background-color: #fff;
+    position: absolute;
+    top: 200px;
+    left: 400px;
+    padding: 30px;
+    border: 1px solid #ccc;
+
+    h1{
+      padding-bottom: 10px;
+    }
+
+    div{
+      font-size: 14px;
+      div{
+        display: inline-block;
+        padding: 4px 10px;
+        border-radius: 4px;
+        border: 1px solid #000;
+        cursor: pointer;
+
+        &:first-child{
+          margin-right:20px;
+          color:#fff;
+          background-color: #77b79f;
+          border: none;
+        }
+        &:hover{
+          background-color: blue;
+        }
+      }
+    }
+  }
+  }
   .bread {
     font-size: 18px;
     font-weight: 700;
